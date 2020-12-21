67356374bb395c98e19a865a7805c52950ae252e
R Gabriel Rodrigues <yemolai@gmail.com>
2020-12-21 20:23:04 -0300

    fix: test name

--- src/components/Profile.test.tsx
+++ src/components/Profile.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Profile } from './Profile'
 
 describe('Profile', () => {
-  it('renders Avatar image', async () => {
+  it('renders User\'s name', async () => {
     render(<Profile username="yemolai" />)
     const textElement = await screen.findByText('Romulo Gabriel Rodrigues')
     expect(textElement).toBeDefined()
