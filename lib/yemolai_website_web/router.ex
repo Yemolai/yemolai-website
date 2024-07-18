f2c445c8aebb32f80eec2e14f46760412a84d181
Gabriel Rodrigues <gabriel.rodrigues@idopterlabs.com.br>
2024-07-18 14:51:41 -0300

    adding projects page + updating main app layout and topbar

--- lib/yemolai_website_web/router.ex
+++ lib/yemolai_website_web/router.ex
@@ -18,6 +18,7 @@ defmodule YemolaiWebsiteWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/projects", PageController, :projects
   end
 
   # Other scopes may use custom stacks.
